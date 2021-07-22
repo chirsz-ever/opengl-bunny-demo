cfe84d6b6ea55306d37f5de7fa26174a15281b69
chirsz-ever <chirsz@foxmail.com>
2021-07-23 02:22:39 +0800

    调整排版
    另外，不用引入 VS 遗留库

--- main.cpp
+++ main.cpp
@@ -8,26 +8,18 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "imgui.h"
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
 
 #include <GLFW/glfw3.h>
 
+#include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl2.h"
 
 #include "materials.h"
 #include "utils.h"
-
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and
-// compatibility with old VS compilers. To link with VS2010-era libraries, VS2015+ requires linking with
-// legacy_stdio_definitions.lib, which we do using this pragma. Your own project should not be affected, as you are
-// likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
 
 static void print_glfw_version();
 static void print_opengl_info();
